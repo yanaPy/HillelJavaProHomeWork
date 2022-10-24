d4f68a84eae3800b6571580b4984df42d7944ab6
Yana Pysarieva <pisarevayasya@gmail.com>
2022-10-24 21:41:30 +0300

    add pack for hw4

---
+++ src/HW4/Car.java
@@ -0,0 +1,22 @@
+package HW4;
+
+public class Car {
+    private final int tankVolume = 60;
+    private int restInTank;
+    private int fuelConsumptionPerHundredKm;
+
+    //    залить полный бак с учетом объема остатка
+    private int fillFullTank() {
+
+    }
+
+    //    определить остаток топлива по преодолении N км
+    private int remainingFuelAfterDrivingN(){
+
+    }
+
+//    определить сколько надо дозаправить топлива для преодоления N км
+    private int determineMuchFuelNeeds(){
+
+    }
+}
