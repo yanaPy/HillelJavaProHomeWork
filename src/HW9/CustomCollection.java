4c82c8a94d2cde5467aecfcc3508827777dd6a2a
Yana Pysarieva <pisarevayasya@gmail.com>
2022-11-14 00:11:36 +0200

    commit hw9

---
+++ src/HW9/CustomCollection.java
@@ -0,0 +1,21 @@
+package HW9;
+
+public interface  CustomCollection<String> {
+    boolean add(String str);
+
+    boolean addAll(Collection strColl);
+
+    boolean delete (int index);
+
+    boolean delete (String str);
+
+    String get(int index);
+
+    boolean contains(String str);
+
+    boolean clear();
+
+    int size();
+
+    boolean trim();
+}
