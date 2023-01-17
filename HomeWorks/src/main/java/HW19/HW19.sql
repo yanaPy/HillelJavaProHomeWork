f8018dde2a7d6e2d7271e4b6176e0f04fec970b9
yana <pisarevayasya@gmail.com>
2023-01-17 20:27:04 +0200

    update HW19

--- HomeWorks/src/main/java/HW19/HW19.sql
+++ HomeWorks/src/main/java/HW19/HW19.sql
@@ -1,22 +1,22 @@
 CREATE DATABASE Student;
 
 CREATE TABLE departments (
-    department_id varchar(64) NOT NULL AUTO_INCREMENT,
+    department_id varchar(64) NOT NULL,
     name varchar(64) NOT NULL,
     manager varchar(64) NOT NULL,
     CONSTRAINT departments_pk PRIMARY KEY (department_id)
 );
 
 -- Table: groups
-CREATE TABLE groups (
-    group_id varchar(64) NOT NULL AUTO_INCREMENT,
-    `group` int NOT NULL,
+CREATE TABLE groupss (
+    group_id varchar(64) NOT NULL,
+    `groupp` int NOT NULL,
     CONSTRAINT groups_pk PRIMARY KEY (group_id)
 );
 
 -- Table: lessons
 CREATE TABLE lessons (
-    lesson_id varchar(64) NOT NULL AUTO_INCREMENT,
+    lesson_id varchar(64) NOT NULL,
     lesson int NOT NULL,
     name varchar(64) NOT NULL,
     teacher varchar(64) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE lessons (
 
 -- Table: scores
 CREATE TABLE scores (
-    scores_id int NOT NULL AUTO_INCREMENT,
+    scores_id int NOT NULL,
     student varchar(255) NOT NULL,
     lesson varchar(64) NOT NULL,
     score varchar(64) NOT NULL
@@ -35,17 +35,17 @@ CREATE TABLE scores (
 
 -- Table: students
 CREATE TABLE students (
-    student_id varchar(64) NOT NULL AUTO_INCREMENT,
-    `group` varchar(64) NOT NULL,
+    student_id varchar(64) NOT NULL,
+    `groupp` varchar(64) NOT NULL,
     fio varchar(255) NOT NULL,
     year_entrance varchar(4) NOT NULL,
-    UNIQUE INDEX payment_data_ak_1 (`group`,fio),
+    UNIQUE INDEX payment_data_ak_1 (`groupp`,fio),
     CONSTRAINT students_pk PRIMARY KEY (student_id)
 );
 
 -- Table: teachers
 CREATE TABLE teachers (
-    teacher_id varchar(64) NOT NULL AUTO_INCREMENT,
+    teacher_id varchar(64) NOT NULL,
     fio varchar(255) NOT NULL,
     department varchar(64) NOT NULL,
     CONSTRAINT teachers_pk PRIMARY KEY (teacher_id)
@@ -61,8 +61,8 @@ ALTER TABLE scores ADD CONSTRAINT lessons_scores FOREIGN KEY lessons_scores (les
     REFERENCES lessons (lesson_id);
 
 -- Reference: students_groups (table: students)
-ALTER TABLE students ADD CONSTRAINT students_groups FOREIGN KEY students_groups (`group`)
-    REFERENCES groups (group_id);
+ALTER TABLE students ADD CONSTRAINT students_groups FOREIGN KEY students_groups (`groupp`)
+    REFERENCES groupss (group_id);
 
 -- Reference: students_scores (table: scores)
 ALTER TABLE scores ADD CONSTRAINT students_scores FOREIGN KEY students_scores (student)
